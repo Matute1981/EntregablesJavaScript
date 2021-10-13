2a2198deedcd9820dc89ab2d2951d7ec38578659
Matute <matiasnlamperti@gmail.com>
2021-10-13 07:57:41 -0300

    Clase 4

--- main.js
+++ main.js
@@ -23,13 +23,80 @@
 
 // Entregable Clase 3 // Algoritmo utilizando un Ciclo
 
-let texto1 = prompt ("Ingresa tu Nombre:").toUpperCase();
-let textoFinal = texto1;
-let texto2 = " ";
+// let texto1 = prompt ("Ingresa tu Nombre:").toUpperCase();
+// let textoFinal = texto1;
+// let texto2 = " ";
 
-while(texto2 != "ESC"){
-    texto2 = prompt("Ingrese su otro Nombre:").toUpperCase();
-    textoFinal = textoFinal + " " + texto2;
-    alert (textoFinal)
+// while(texto2 != "ESC"){
+//     texto2 = prompt("Ingrese su otro Nombre:").toUpperCase();
+//     textoFinal = textoFinal + " " + texto2;
+//     alert (textoFinal)
 
+// }
+
+// Entregable Clase 4 // Programción avanzada con funciones
+// Simulador intercativo (Costo total de prod seleccionado por usuario) + Funciones relacionadas
+
+let total = 0;
+
+function carrito() {
+    do{
+        let productos = prompt("¿Qué producto querés comprar, remera, gorra o ambos?", "Ej: ambos");
+
+        let cantidad = parseInt(prompt("Cuantos querés comprar?", "0"));
+
+        let precio = 0;
+    
+        switch (productos){
+            case "remera":
+                precio = 2000;
+                break;
+            case "gorra":
+                precio = 1200;
+                break;
+            case "ambos":
+                precio = 3200;
+                break;
+            
+            default:
+                alert("Algunos de los datos ingresados son incorrectos");
+                precio = 0;
+                cantidad = 0;
+        }
+        total = total + precio * cantidad;
+        otroProducto = confirm("¿Querés agregar otro prodcuto?");
+
+    }while (otroProducto);
 }
+
+    function sumarIva (total){
+            total = total * 1.21;
+            return total;
+        }
+
+    function aplicarPromocion(total){
+        if(total >= 3872){
+            total = total * 0.85;
+            alert("Tu compra tiene un descuento del 15% y tu subtotal es de $ " + total + " + IVA");
+        }
+        return total;
+    }
+
+    function calcularEnvio(total){
+        let confirmacion = confirm("Querés envío a domicilio?");
+            
+            if(confirmacion && total >= 3200){
+                alert("El envío es gratuito. El costo total de tu compra es de $ " + total + " IVA incluido");
+            }else if (confirmacion && total < 3200 && total != 0){
+                alert("Tu costo de envío es de $ 600 final. El total de la operación es de $ " + (total + 600) + " IVA incluido");
+            }
+            else{
+                alert("El total de tu compra es de $ " + total + " IVA incluido");
+            }
+            return total;
+    }
+
+carrito();
+calcularEnvio(aplicarPromocion(sumarIva(total)));
+
+
