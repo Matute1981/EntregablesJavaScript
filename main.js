7419a0f4825acca069fd3ddf6b7f46a495a62f3e
Matute <matiasnlamperti@gmail.com>
2021-10-19 08:46:59 -0300

    Arrays

--- main.js
+++ main.js
@@ -101,6 +101,83 @@
 
 // Entregable Clase 5 // Incorporar objetos
 
+// let producto = 0;
+// let cantidadProducto = 0;
+// let precio = 0;
+
+// class Orden{
+//     constructor(producto, precio, cantidad){
+//         this.producto = producto,
+//         this.precio = precio,
+//         this.cantidad = cantidad,
+//         this.envio = 0,
+//         this.subTotal = 0,
+//         this.total = 0
+//     }
+
+//     calcularSubTotal(){
+//         this.subTotal = this.precio * this.cantidad;
+//     }
+
+//     calcularIva(){
+//         return this.subTotal * 0.21;
+//     }
+
+//     calcularEnvio(){
+//         if (this.subTotal >= 3200){
+//             this.envio = 0;
+//         }else {
+//             this.envio = 600;
+//         }
+//     }
+
+//     calcularTotal(){
+//         this.total = this.subTotal + this.envio + this.calcularIva();
+//     }
+// }
+
+// function ordenColocada(){
+//     while(!producto || producto == 0 || producto > 4){
+//         producto = parseInt(prompt("¿Qué producto desea sumar al carrito? \n 1: Gorra ($1200) \n 2: Remera ($2500) \n 3: CD - El veneno de tu Soledad ($1400) \n 4: CD - El momento Indicado ($1700) "))
+//     }
+//     switch(producto){
+//         case 1 :
+//             producto = "Gorra";
+//             precio = 1200;
+//             break;
+//         case 2 :
+//             producto = "Remera";
+//             precio = 2500;
+//             break;
+//         case 3 :
+//             producto = "CD - El veneno de tu Soledad";
+//             precio = 1400;
+//             break;
+//         case 4 :
+//             producto = "CD - El momento Indicado";
+//             precio = 1700;
+//             break;
+//     }
+
+//     while(!cantidadProducto || cantidadProducto == 0){
+//         cantidadProducto = parseInt(prompt("Producto seleccionado: " + producto + "\n Indicar la cantidad requerida (sólo números):"));
+//     }
+//     return new Orden (producto, precio, cantidadProducto)
+// }
+
+// alert("Estamos listos para tu compra!!!");
+
+// const pedido = ordenColocada();
+
+// pedido.calcularSubTotal();
+// pedido.calcularIva();
+// pedido.calcularEnvio();
+// pedido.calcularTotal();
+
+// alert("Resumen de la Orden: \n" + "- " + pedido.producto + " x " + pedido.cantidad + ": $" + pedido.precio * pedido.cantidad + "\n" + "- " + "Total IVA: $" + pedido.calcularIva() + "\n" + "- " + "Costo de Envío: $" + pedido.envio + "\n" + "Total de la compra final: $" + pedido.total);
+
+// Entregable Clase 6 // Incorporar Arrays
+
 let producto = 0;
 let cantidadProducto = 0;
 let precio = 0;
@@ -136,45 +213,73 @@ class Orden{
     }
 }
 
-function ordenColocada(){
-    while(!producto || producto == 0 || producto > 4){
-        producto = parseInt(prompt("¿Qué producto desea sumar al carrito? \n 1: Gorra ($1200) \n 2: Remera ($2500) \n 3: CD - El veneno de tu Soledad ($1400) \n 4: CD - El momento Indicado ($1700) "))
+const productos = [];
+
+const producto1 = new Orden ("Gorra","1200");
+productos.push(producto1);
+const producto2 = new Orden ("Remera","2500");
+productos.push(producto2);
+const producto3 = new Orden ("CD - El veneno de tu Soledad","1400");
+productos.push(producto3);
+const producto4 = new Orden ("CD - El momento Indicado","1700");
+productos.push(producto4);
+
+// Entregable Complementario // Ordenar un Array de Objetos (de mayor a menor precio)
+
+productos.sort((a,b)=>{
+    if (a.precio > b.precio){
+        return -1;
     }
-    switch(producto){
-        case 1 :
-            producto = "Gorra";
-            precio = 1200;
-            break;
-        case 2 :
-            producto = "Remera";
-            precio = 2500;
-            break;
-        case 3 :
-            producto = "CD - El veneno de tu Soledad";
-            precio = 1400;
-            break;
-        case 4 :
-            producto = "CD - El momento Indicado";
-            precio = 1700;
-            break;
+    if (a.precio < b.precio){
+        return 1;
+    }else{
+        return 0;
     }
-
-    while(!cantidadProducto || cantidadProducto == 0){
-        cantidadProducto = parseInt(prompt("Producto seleccionado: " + producto + "\n Indicar la cantidad requerida (sólo números):"));
-    }
-    return new Orden (producto, precio, cantidadProducto)
-}
-
-alert("Estamos listos para tu compra!!!");
-
-const pedido = ordenColocada();
-
-pedido.calcularSubTotal();
-pedido.calcularIva();
-pedido.calcularEnvio();
-pedido.calcularTotal();
-
-alert("Resumen de la Orden: \n" + "- " + pedido.producto + " x " + pedido.cantidad + ": $" + pedido.precio * pedido.cantidad + "\n" + "- " + "Total IVA: $" + pedido.calcularIva() + "\n" + "- " + "Costo de Envío: $" + pedido.envio + "\n" + "Total de la compra final: $" + pedido.total);
+});
+console.log (productos);
 
 
+// Clase 7 ejercicios de la clase
 
+// localStorage.setItem('bienvenida','Hola Coder');
+// localStorage.setItem('esValido', 'true');
+// localStorage.setItem('unNumero', 20);
+
+// let mensaje = localStorage.getItem('bienvenida');
+// let bandera = localStorage.getItem('esValido');
+// let numero = localStorage.getItem('unNumero');
+
+// for(let i=0; i<localStorage.length; i++){
+//     let clave = localStorage.key(i);
+
+//     console.log("Clave: " + clave);
+//     console.log("Valor: " + localStorage.getItem(clave));
+
+// }
+
+// localStorage.removeItem('bienvenida');
+// localStorage.clear();
+
+// Ejemplo de multiplicación
+
+// const multiplicar = (a, b) => a * b;
+// const guardarLocal = (clave, valor) => {localStorage.setItem(clave, valor)};
+
+// let ingresarNumero = parseInt(prompt("Ingresa un número: "));
+
+// for(let i=1; i <= 10; i++){
+//     guardarLocal(i, multiplicar(parseInt(ingresarNumero), i));
+// }
+
+// Ejemplo JSON
+
+// const producto1 = {id:2, producto: "Gorra"};
+// const enJSON = JSON.stringify(producto1);
+
+// localStorage.setItem("Elemento1", enJSON);
+
+// const producto2 = JSON.parse(localStorage.getItem("Elemento1"));
+
+// console.log(producto2);
+
+// localStorage.clear();
